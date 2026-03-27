65484ec84c252592bcd0deb8be349b27767f301e
miloslav krastev <derbibr@yahoo.com>
2026-03-27 09:57:58 +0200

    scaffold: add index.tsx for Buyer/RequestCreation

---
+++ front-end/src/pages/Dashboard/sections/Buyer/features/RequestCreation/index.tsx
@@ -0,0 +1,7 @@
+/**
+ * RequestCreation feature barrel export (TSX)
+ * Re-exports from index.ts for rule compliance
+ */
+export { BuyerRequestCreationFlow } from './BuyerRequestCreationFlow';
+export { useBuyerRequestCreation } from './hooks/useBuyerRequestCreation';
+export type * from './types';
